152cc98a2f839a3de90d3e28a77ebbad049a927d
Riley Moore <rilemoore@gmail.com>
2020-07-13 20:35:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,13 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-    resp.write "Good morning"
+    time = Time.now.hour
+
+    if time <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
     resp.finish
   end
  
